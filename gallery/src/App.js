6444c7ac401e99422bcc6c409fdc0177c9ffcd05
Lincoln Atkinson <LINCOLN.ATKINSON2478@STU.MTEC.EDU>
2023-11-09 08:39:36 -0700

    added click to images

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [images, setImages] = useState([]);
   const [custom, setCustom] = useState('');
   const [focus, setFocus] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
   const ky = useRef(null);  
   useEffect(() => {
     const button = document.querySelector('.enter');
@@ -59,26 +60,45 @@ function App() {
       search(custom);
     }
   }
+  const openImage = (key, img, alt) => {
+    setSelectedImage({key, img, alt})
+    const main = document.querySelector('.mainPage');
+    main.classList.add('hide');
+  }
+  const closeImage = () => {
+    setSelectedImage(null);
+    const main = document.querySelector('.mainPage');
+    main.classList.remove('hide');
+  }
 
   return (
-    <div className='mainPage'>
-      <div className='nav'>
-        <button onClick={() => search('nature')}>Nature</button>
-        <button onClick={() => search('city')}>City</button>
-        <button onClick={() => search('lake')}>Lake</button>
-        <button onClick={() => search('sky')}>Sky</button>
-        <button onClick={() => search('space')}>Space</button>
-        <button onClick={() => search('animals')}>Animals</button>
-        <input type='text' placeholder='Search...' value={custom} onChange={(e) => setCustom(e.target.value)}
-         onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} onKeyDown={blur}></input>
-        <button className='enter' onClick={() => click()} ref={ky}>Enter</button>
+    <>
+      <div className='mainPage'>
+        <div className='nav'>
+          <button onClick={() => search('nature')}>Nature</button>
+          <button onClick={() => search('city')}>City</button>
+          <button onClick={() => search('lake')}>Lake</button>
+          <button onClick={() => search('sky')}>Sky</button>
+          <button onClick={() => search('space')}>Space</button>
+          <button onClick={() => search('animals')}>Animals</button>
+          <input type='text' placeholder='Search...' value={custom} onChange={(e) => setCustom(e.target.value)}
+          onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} onKeyDown={blur}></input>
+          <button className='enter' onClick={() => click()} ref={ky}>Enter</button>
+        </div>
+        <div className='container'>
+          {images.map((image) => (
+            <img key={image.id} src={image.urls.regular} onClick={() => openImage(image.id, image.urls.regular, image.description)} alt={image.description} className='image'/>
+          ))}
+        </div>
       </div>
-      <div className='container'>
-        {images.map((image) => (
-          <img key={image.id} src={image.urls.regular} alt={image.description} className='image'/>
-        ))}
-      </div>
-    </div>
+      {selectedImage && (
+        <div className='single'>
+          <img key={selectedImage.key} src={selectedImage.img} alt={selectedImage.alt} className='selected' />
+          <button onClick={closeImage} className='exit'>Exit</button>
+        </div>
+      )}
+    </>
+    
   );
 }
 
